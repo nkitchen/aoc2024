7ae263497da0e5df8456e652a7a91f8dc039bb3d
Nathan Kitchen <nathan.kitchen@gmail.com>
2025-01-15 07:42:40 -0800

    day 15, part 2

--- clojure/day15.clj
+++ clojure/day15.clj
@@ -5,8 +5,8 @@
   (:require
    aoc
    [clojure.string :as str]
-   [quil.core :as q]
-   [quil.middleware :as m]
+   ;[quil.core :as q]
+   ;[quil.middleware :as m]
    [nextjournal.clerk :as clerk]))
 
 
@@ -217,112 +217,285 @@
 
 ;; ## Part 2
 ;;
-;; ![](https://media1.tenor.com/m/CxROZVyqKIwAAAAd/aint-nobody-got-time-for-that-sweet-brown.gif)
-;;
-;; Maybe some other time...
+;; Nathan's contribution
 ;;
 
+(def unscaled-example-2 "#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
 
+<vv<<^^<<^^")
 
+(defn map-gps [f points]
+  (for [pos points
+        :let [x (mod pos 100) y (quot pos 100)]
+        [x' y'] (f x y)]
+    (+ x' (* y' 100))))
 
+(defn two-cells [x y]
+  [[(* 2 x) y]
+   [(inc (* 2 x)) y]])
 
+(defn left-cell [x y]
+  [[(* 2 x) y]])
 
-;; ## Animation
-;;
-;; The animation for the real input would be too long and too boring.
-;; Or waaaay too fast.\
-;; Let's do the animation for the first example from the task.
-;;
-{:nextjournal.clerk/visibility {:result :hide}}
+(defn resize-map [[state moves]]
+  (let [{:keys [walls boxes bot]} state]
+    [{:walls (into #{} (map-gps two-cells walls))
+      :boxes (into #{} (map-gps left-cell boxes))
+      :bot (first (map-gps left-cell [bot]))}
+     moves]))
 
-(def states
-  (let [ex (parse-data (aoc/read-input "15_test"))
-        [state moves] ex]
-    (reductions move state moves)))
+(defn try-move-2
+  ([{:keys [walls boxes bot] :as state} dir]
+   ; Moving the bot
+   (let [bot' (+ bot dir)
+         state' (assoc state :bot bot')]
+     (cond
+       (walls bot') nil
 
-(defn setup []
-  (q/frame-rate 15)
-  (q/no-stroke)
-  (q/ellipse-mode :corner)
-  states)
+       (and (#{-100 100} dir) (boxes bot'))
+       ; []
+       ; @^
+       (try-move-2 state' dir bot')
 
-(defn update-state [states]
-  (rest states))
+       (and (#{-100 100} dir) (boxes (dec bot')))
+       ; []
+       ;  @^
+       (try-move-2 state' dir (dec bot'))
 
-(defn draw-walls [walls]
-  (q/fill 12 87 11)
-  (doseq [gps walls]
-    (let [x (mod gps 100)
-          y (quot gps 100)]
-      (q/rect (+ 0.1 x) (+ 0.1 y) 0.8 0.8))))
+       (and (= dir 1) (boxes bot'))
+       ; >@[]
+       (try-move-2 state' dir bot')
 
-(defn draw-boxes [boxes]
-  (q/fill 255 255 102)
-  (doseq [gps boxes]
-    (let [x (mod gps 100)
-          y (quot gps 100)]
-      (q/ellipse (+ 0.1 x) (+ 0.1 y) 0.8 0.8))))
+       (and (= dir -1) (boxes (dec bot')))
+       ; []@<
+       (try-move-2 state' dir (dec bot'))
 
-(defn draw-bot [bot]
-  (q/fill 180 20 20)
-  (let [x (mod bot 100)
-        y (quot bot 100)]
-    (q/ellipse x y 1 1)))
+       :else state')))
 
-(defn draw-state [states]
-  (q/scale 75)
-  (q/background 15 15 35)
-  (when (empty? states)
-    (q/exit))
-  (let [{:keys [walls boxes bot]} (first states)]
-    (draw-walls walls)
-    (draw-boxes boxes)
-    (draw-bot bot))
-  (q/save-frame "/tmp/imgs/day15-###.jpg"))
+  ([{:keys [walls boxes bot] :as state} dir pos]
+   ; Moving a box from pos
+   (let [lpos pos
+         rpos (inc pos)
+         lpos' (+ lpos dir)
+         rpos' (+ rpos dir)]
+     (cond
+       (walls lpos') nil
+       (walls rpos') nil
 
-(comment
-  (q/sketch
-   :size [750 750]
-   :setup #'setup
-   :update #'update-state
-   :draw #'draw-state
-   :middleware [m/fun-mode]))
+       (and (#{-100 100} dir) (boxes lpos'))
+       ;  []
+       ; ^[]
+       (some-> state
+               (update :boxes disj lpos)
+               (try-move-2 dir lpos')
+               (update :boxes conj lpos'))
 
+       (and (#{-100 100} dir) (boxes (dec lpos')) (not (boxes rpos')))
+       ; []
+       ;  []^
+       (some-> state
+               (update :boxes disj lpos)
+               (try-move-2 dir (dec lpos'))
+               (update :boxes conj lpos'))
 
-;; To make a video from the frames created above, I'm using the following command:\
-;; `ffmpeg -framerate 15 -i /tmp/imgs/day15-%03d.jpg -c:v libx264 -pix_fmt yuv420p imgs/day15.mp4`
-;;
-;; And the result is:
-;;
-^{:nextjournal.clerk/visibility {:code :hide
-                                 :result :show}}
-(clerk/html
- [:video {:controls true}
-  [:source {:src "https://i.imgur.com/fNdzHpx.mp4"
-            :type "video/mp4"}]
-  "Your browser does not support the video tag."])
+       (and (#{-100 100} dir) (boxes rpos') (not (boxes (dec lpos'))))
+       ;   []
+       ; ^[]
+       (some-> state
+               (update :boxes disj lpos)
+               (try-move-2 dir rpos')
+               (update :boxes conj lpos'))
 
+       (and (#{-100 100} dir) (boxes (dec lpos')) (boxes rpos'))
+       ; [][]
+       ;  []^
+       (some-> state
+               (update :boxes disj lpos)
+               (try-move-2 dir (dec lpos'))
+               (try-move-2 dir rpos')
+               (update :boxes conj lpos'))
 
+       (and (= dir -1) (boxes (dec lpos')))
+       ; [][]<
+       (some-> state
+               (update :boxes disj lpos)
+               (try-move-2 dir (dec lpos'))
+               (update :boxes conj lpos'))
 
+       (and (= dir 1) (boxes rpos'))
+       ; >[][]
+       (some-> state
+               (update :boxes disj lpos)
+               (try-move-2 dir rpos')
+               (update :boxes conj lpos'))
 
+       :else (-> state
+                 (update :boxes disj lpos)
+                 (update :boxes conj lpos'))))))
 
+(defn move-2 [state dir]
+  (or (try-move-2 state dir) state))
 
-;; ## Conclusion
-;;
-;; Sokoban, anybody?
-;;
-;; Today's highlights:
-;; - `str/join`: concatenate a sequence
+(defn part-2 [resized-data]
+  (let [[state moves] resized-data]
+    (->> (reduce move-2 state moves)
+         :boxes
+         (reduce +))))
+  
+(def example-data-2 (resize-map (parse-data unscaled-example-2)))
 
+(def example-2-result
+  (let [[state moves] example-data-2]
+    (reduce move-2 state moves)))
 
+(def unscaled-larger-example "##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
 
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^")
 
+(def larger-example-data (resize-map (parse-data unscaled-larger-example)))
 
+(defn render [{:keys [walls boxes bot] :as state}]
+  (let [xmax (apply max (for [pos walls] (mod pos 100)))
+        ymax (apply max (for [pos walls] (quot pos 100)))]
+    (apply str
+          (flatten
+            (for [y (range 0 (inc ymax))]
+              (concat
+                (for [ x (range 0 (inc xmax))
+                      :let [pos (+ x (* 100 y))]]
+                  (cond
+                    (walls pos) \#
+                    (boxes pos) \[
+                    (boxes (dec pos)) \]
+                    (= pos bot) \@
+                    :else \.))
+                [\newline]))))))
 
+(defn debug-move [state move]
+  (let [state' (move-2 state move)]
+    (spit "debug/day15.txt" (str move \newline) :append true)
+    (spit "debug/day15.txt" (str (render state') \newline) :append true)
+    state'))
 
+(def debug-larger
+  (let [[state moves] larger-example-data]
+    (reduce debug-move state moves)))
 
+(part-2 larger-example-data)
 
-^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
-(defn -main [input]
-  (let [data (parse-data input)]
-    (part-1 data)))
+(part-2 (resize-map data))
+
+;;; ## Animation
+;;;
+;;; The animation for the real input would be too long and too boring.
+;;; Or waaaay too fast.\
+;;; Let's do the animation for the first example from the task.
+;;;
+;{:nextjournal.clerk/visibility {:result :hide}}
+;
+;(def states
+;  (let [ex (parse-data (aoc/read-input "15_test"))
+;        [state moves] ex]
+;    (reductions move state moves)))
+;
+;(defn setup []
+;  (q/frame-rate 15)
+;  (q/no-stroke)
+;  (q/ellipse-mode :corner)
+;  states)
+;
+;(defn update-state [states]
+;  (rest states))
+;
+;(defn draw-walls [walls]
+;  (q/fill 12 87 11)
+;  (doseq [gps walls]
+;    (let [x (mod gps 100)
+;          y (quot gps 100)]
+;      (q/rect (+ 0.1 x) (+ 0.1 y) 0.8 0.8))))
+;
+;(defn draw-boxes [boxes]
+;  (q/fill 255 255 102)
+;  (doseq [gps boxes]
+;    (let [x (mod gps 100)
+;          y (quot gps 100)]
+;      (q/ellipse (+ 0.1 x) (+ 0.1 y) 0.8 0.8))))
+;
+;(defn draw-bot [bot]
+;  (q/fill 180 20 20)
+;  (let [x (mod bot 100)
+;        y (quot bot 100)]
+;    (q/ellipse x y 1 1)))
+;
+;(defn draw-state [states]
+;  (q/scale 75)
+;  (q/background 15 15 35)
+;  (when (empty? states)
+;    (q/exit))
+;  (let [{:keys [walls boxes bot]} (first states)]
+;    (draw-walls walls)
+;    (draw-boxes boxes)
+;    (draw-bot bot))
+;  (q/save-frame "/tmp/imgs/day15-###.jpg"))
+;
+;(comment
+;  (q/sketch
+;   :size [750 750]
+;   :setup #'setup
+;   :update #'update-state
+;   :draw #'draw-state
+;   :middleware [m/fun-mode]))
+;
+;
+;;; To make a video from the frames created above, I'm using the following command:\
+;;; `ffmpeg -framerate 15 -i /tmp/imgs/day15-%03d.jpg -c:v libx264 -pix_fmt yuv420p imgs/day15.mp4`
+;;;
+;;; And the result is:
+;;;
+;^{:nextjournal.clerk/visibility {:code :hide
+;                                 :result :show}}
+;(clerk/html
+; [:video {:controls true}
+;  [:source {:src "https://i.imgur.com/fNdzHpx.mp4"
+;            :type "video/mp4"}]
+;  "Your browser does not support the video tag."])
+;
+;
+;
+;
+;
+;
+;;; ## Conclusion
+;;;
+;;; Sokoban, anybody?
+;;;
+;;; Today's highlights:
+;;; - `str/join`: concatenate a sequence
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+;(defn -main [input]
+;  (let [data (parse-data input)]
+;    (part-1 data)))
